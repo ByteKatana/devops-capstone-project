b096d3dc1da5b32f8b28ae2c9213ca077c036755
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2024-02-07 20:40:02 -0500

    test fix

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -192,11 +192,8 @@ class TestAccountService(TestCase):
 
     def test_cors_security(self):
         """It should return a CORS header"""
-        response = self.cl
-        Flask-Cors
-        
-        
-        Flask-Corsient.get('/', environ_overrides=HTTPS_ENVIRON)
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+        
